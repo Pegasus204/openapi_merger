a6a1ec0877bdeff308529bff61bbd1d233a22f82
Fukaya Temma <tenma.fukaya@kyash.co>
2020-01-27 14:58:27 +0900

    remove unnecessary code

--- lib/openapi_merger.rb
+++ lib/openapi_merger.rb
@@ -1,6 +1,1 @@
 require "openapi_merger/version"
-
-module OpenapiMerger
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- spec/openapi_merger_spec.rb
+++ spec/openapi_merger_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe OpenapiMerger do
   it "has a version number" do
     expect(OpenapiMerger::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
