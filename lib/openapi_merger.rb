aec6b389b6d1e4013b77342626b51dedfce1f92e
Fukaya Temma <tenma.fukaya@kyash.co>
2020-01-28 16:34:02 +0900

    input -> parse -> output to file

--- lib/openapi_merger.rb
+++ lib/openapi_merger.rb
@@ -6,3 +6,29 @@ require 'openapi_merger/error'
 require 'openapi_merger/input'
 require 'openapi_merger/output'
 require 'openapi_merger/version'
+
+module OpenapiMerger
+  class << self
+    def call
+      parsed_input = OpenapiMerger::Input.new(options).call
+      OpenapiMerger::Output.new(options).call(parsed_input)
+    end
+
+    private
+
+    def options
+      {
+        input: input,
+        output: output
+      }.compact
+    end
+
+    def input
+      ENV['INPUT_FILE']
+    end
+
+    def output
+      ENV['OUTPUT_FILE']
+    end
+  end
+end
