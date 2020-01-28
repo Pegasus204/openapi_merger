fdca76ea52aa179117412443dc67aa77d5645ba5
Fukaya Temma <tenma.fukaya@kyash.co>
2020-01-28 13:57:16 +0900

    prepare for brand new change

--- lib/openapi_merger.rb
+++ lib/openapi_merger.rb
@@ -1,22 +1,2 @@
 require "openapi_merger/error"
-require "openapi_merger/base"
-require "openapi_merger/json_merger"
-require "openapi_merger/yaml_merger"
 require "openapi_merger/version"
-
-module OpenapiMerger
-  class << self
-    def call(options)
-      return NoInputFileError unless options[:input]
-
-      case File.extname options[:input].downcase
-      when '.yaml', '.yml'
-        OpenapiMerger::YAMLMerger.new(options).call
-      when '.json'
-        OpenapiMerger::JSONMerger.new(options).call
-      else
-        UnexpectedInputFileError
-      end
-    end
-  end
-end
--- lib/openapi_merger/error.rb
+++ lib/openapi_merger/error.rb
@@ -1,4 +1,2 @@
 module OpenapiMerger
-  class NoInputFileError < StandardError; end
-  class UnexpectedInputFileError < StandardError; end
 end
