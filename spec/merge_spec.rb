d945aad564354ba0d77e2c2c531ecbef31522618
Fukaya Temma <ride.or.die.2215@gmail.com>
2020-01-29 01:16:23 +0900

    add test

--- spec/merge_spec.rb
+++ spec/merge_spec.rb
@@ -4,9 +4,24 @@ require 'spec_helper'
 
 RSpec.describe OpenapiMerger::Merge do
   describe '#call' do
-    let(:target) { OpenapiMerger::Merge.new(options) }
+    let(:target) { described_class.new(options) }
+    let(:origin) { OpenapiMerger::Input.new(options).call }
+    let(:expected) do
+      {
+        'test' => {
+          'merge' => {
+            'yaml' => 'This is merged YAML'
+          }
+        }
+      }
+    end
 
     context 'Replace $ref flag to relational file' do
+      let(:options) { { input: 'spec/fixtures/dummy.yml' } }
+
+      it 'Returns merged object' do
+        expect(target.call(origin)).to eq expected
+      end
     end
   end
 end
